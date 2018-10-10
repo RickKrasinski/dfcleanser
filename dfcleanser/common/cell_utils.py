39be5a751a4a9881250194bddd1480a4b81a3f4a
RickKrasinski <rickkrasinski@stny.rr.com>
2018-10-09 23:36:46 -0400

    Add files via upload

--- dfcleanser/common/cell_utils.py
+++ dfcleanser/common/cell_utils.py
@@ -22,63 +22,63 @@ import dfcleanser.common.help_utils as dfchelp
 # -----------------------------------------------------------------------
 """
 
-CLEANSING_CODE_CELL         =   "from dfcleanser.data_cleansing.data_cleansing_control import display_data_cleansing\ndisplay_data_cleansing(0)"
-CLEANSING_HELP_CELL         =   "<div align='left' id='CleansingHelp'/><p class='DCTitleChapterstext'></p></div>"
-CLEANSING_TITLE_CELL        =   "<div align='left' id='DCDataCleansing'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/dataCleansing1.jpg' width='80' align='left'/></div><div><image width='10'></div><div><h2>&nbsp;&nbsp;&nbsp;Data Cleansing : </h2><a name='DataCleansing'></a></div></div>"
+CLEANSING_CODE_CELL         =   'from dfcleanser.data_cleansing.data_cleansing_control import display_data_cleansing\\ndisplay_data_cleansing(0)'
+CLEANSING_HELP_CELL         =   '<div align="left" id="CleansingHelp"/><p class="DCTitleChapterstext"></p></div>'
+CLEANSING_TITLE_CELL        =   '<div align="left" id="DCDataCleansing"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/dataCleansing1.jpg" width="80" align="left"/></div><div><image width="10"></div><div><h2>&nbsp;&nbsp;&nbsp;Data Cleansing : </h2><a name="DataCleansing"></a></div></div>'
 
-EXPORT_CODE_CELL            =   "from dfcleanser.data_export.data_export_control import display_export_forms\ndisplay_export_forms(0)"
-EXPORT_HELP_CELL            =   "<div align='left' id='ExportHelp'/><p class='DCTitleChapterstext'></p></div>"
-EXPORT_TITLE_CELL           =   "<div align='left' id='DCDataExport'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/dataExport.png' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h2>&nbsp;&nbsp;&nbsp;Data Export</h2><a name='DataExport'></a></div></div>"
+EXPORT_CODE_CELL            =   'from dfcleanser.data_export.data_export_control import display_export_forms\\ndisplay_export_forms(0)'
+EXPORT_HELP_CELL            =   '<div align="left" id="ExportHelp"/><p class="DCTitleChapterstext"></p></div>'
+EXPORT_TITLE_CELL           =   '<div align="left" id="DCDataExport"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/dataExport.png" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h2>&nbsp;&nbsp;&nbsp;Data Export</h2><a name="DataExport"></a></div></div>'
 
-IMPORT_CODE_CELL            =   "from dfcleanser.data_import.data_import_control import display_import_forms\ndisplay_import_forms(0)"
-IMPORT_HELP_CELL            =   "<div align='left' id='ImportHelp'/><p class='DCTitleChapterstext'></p></div>"
-IMPORT_TITLE_CELL           =   "<div align='left' id='DCDataImport'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/dataImport.png' width='80' align='left'/></div><div><image width='10'></div><div><h2>&nbsp;&nbsp;&nbsp;Data Import : </h2><a name='DataImport'></a></div></div>"
+IMPORT_CODE_CELL            =   'from dfcleanser.data_import.data_import_control import display_import_forms\\ndisplay_import_forms(0)'
+IMPORT_HELP_CELL            =   '<div align="left" id="ImportHelp"/><p class="DCTitleChapterstext"></p></div>'
+IMPORT_TITLE_CELL           =   '<div align="left" id="DCDataImport"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/dataImport.png" width="80" align="left"/></div><div><image width="10"></div><div><h2>&nbsp;&nbsp;&nbsp;Data Import : </h2><a name="DataImport"></a></div></div>'
 
-INSPECT_CODE_CELL           =   "from dfcleanser.data_inspection.data_inspection_control import display_data_inspection\ndisplay_data_inspection(0)"
-INSPECT_HELP_CELL           =   "<div align='left' id='InspectionHelp'/><p class='DCTitleChapterstext'></p></div>"
-INSPECT_TITLE_CELL          =   "<div align='left' id='DCDataInspection'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/dataInspection.png' width='80' align='left'/></div><div><image width='10'></div><div><h2>&nbsp;&nbsp;&nbsp;Data Inspection : </h2><a name='DataInspection'></a></div></div>"
+INSPECT_CODE_CELL           =   'from dfcleanser.data_inspection.data_inspection_control import display_data_inspection\\ndisplay_data_inspection(0)'
+INSPECT_HELP_CELL           =   '<div align="left" id="InspectionHelp"/><p class="DCTitleChapterstext"></p></div>'
+INSPECT_TITLE_CELL          =   '<div align="left" id="DCDataInspection"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/dataInspection.png" width="80" align="left"/></div><div><image width="10"></div><div><h2>&nbsp;&nbsp;&nbsp;Data Inspection : </h2><a name="DataInspection"></a></div></div>'
 
-PANDAS_TITLE_CELL           =   "<div><table align='center'><tr><td><img src='https://rickkrasinski.github.io/dfcleanser/graphics/pandas.png' style='width:120px; height:120px;'></td><td style='margin-left:200px;'><p style='text-align:left;' id='mainTitle'><font size='6'>Pandas Dataframe Cleanser</font></p><p id='titleComment'><font size='3'>A utility to prepare your pandas dataframe for data analytics.</font></p></td><td><img src='https://rickkrasinski.github.io/dfcleanser/graphics/dataCleansing.png' style='width:120px;height:120px;'></td></tr></table></div>"
+PANDAS_TITLE_CELL           =   '<div><table align="center"><tr><td><img src="https://rickkrasinski.github.io/dfcleanser/graphics/pandas.png" style="width:120px; height:120px;"></td><td style="margin-left:200px;"><p style="text-align:left;" id="mainTitle"><font size="6">Pandas Dataframe Cleanser</font></p><p id="titleComment"><font size="3">A utility to prepare your pandas dataframe for data analytics.</font></p></td><td><img src="https://rickkrasinski.github.io/dfcleanser/graphics/dataCleansing.png" style="width:120px;height:120px;"></td></tr></table></div>'
 
-SCRIPT_SECTION_TITLE_CELL   =   "<div align='left' id='ScriptingMode'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/ScriptMode.jpg' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h2 class='DCTitleChaptersh2'>&nbsp;&nbsp;&nbsp;Scripting Mode</h2><a name='Script'></a></div></div></div><br>"
+SCRIPT_SECTION_TITLE_CELL   =   '<div align="left" id="ScriptingMode"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/ScriptMode.jpg" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h2 class="DCTitleChaptersh2">&nbsp;&nbsp;&nbsp;Scripting Mode</h2><a name="Script"></a></div></div></div><br>'
 
-SCRIPTING_CODE_CELL         =   "from dfcleanser.scripting.data_scripting_control import display_data_scripting\ndisplay_data_scripting(0)"
-SCRIPTING_HELP_CELL         =   "<div align='left' id='ScriptingHelp'/><p class='DCTitleChapterstext'></p></div>"
-SCRIPTING_TITLE_CELL        =   "<div align='left' id='DCDataScripting'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/Script.png' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h3>&nbsp;Dataframe Cleanser Scripting</h3><a name='Script'></a></div></div>"
+SCRIPTING_CODE_CELL         =   'from dfcleanser.scripting.data_scripting_control import display_data_scripting\\ndisplay_data_scripting(0)'
+SCRIPTING_HELP_CELL         =   '<div align="left" id="ScriptingHelp"/><p class="DCTitleChapterstext"></p></div>'
+SCRIPTING_TITLE_CELL        =   '<div align="left" id="DCDataScripting"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/Script.png" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h3>&nbsp;Dataframe Cleanser Scripting</h3><a name="Script"></a></div></div>'
 
-SW_UTILS_SECTION_TITLE_CELL =   "<div align='left' id='SWUtilities'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/SWUtilities.png' width='80' align='left' style='border: 0px; margin-right: 6px; margin-left: 0px'/></div><div><image width='10'></div><div><image width='10'><h2>&nbsp;&nbsp;&nbsp;Software Utilities</h2></div></div>"
+SW_UTILS_SECTION_TITLE_CELL =   '<div align="left" id="SWUtilities"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/SWUtilities.png" width="80" align="left" style="border: 0px; margin-right: 6px; margin-left: 0px"/></div><div><image width="10"></div><div><image width="10"><h2>&nbsp;&nbsp;&nbsp;Software Utilities</h2></div></div>'
 
-SW_UTILS_DS_CODE_CELL       =   "from dfcleanser.sw_utilities.sw_utility_control import process_sw_utilities\nprocess_sw_utilities(0)"
-SW_UTILS_DS_HELP_CELL       =   "<div align='left' id='DCListUtilityHelp'/><p class='DCTitleChapterstext'></p></div>"
-SW_UTILS_DS_TITLE_CELL      =   "<div align='left' id='DCSWDataStructUtility'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/ListBuild.png' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h3>&nbsp;&nbsp;&nbsp;Common Data Structures Utility</h3></div></div><br>"
+SW_UTILS_DS_CODE_CELL       =   'from dfcleanser.sw_utilities.sw_utility_control import process_sw_utilities\\nprocess_sw_utilities(0)'
+SW_UTILS_DS_HELP_CELL       =   '<div align="left" id="DCListUtilityHelp"/><p class="DCTitleChapterstext"></p></div>'
+SW_UTILS_DS_TITLE_CELL      =   '<div align="left" id="DCSWDataStructUtility"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/ListBuild.png" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h3>&nbsp;&nbsp;&nbsp;Common Data Structures Utility</h3></div></div>'
 
-SW_UTILS_DC_CODE_CELL       =   "from dfcleanser.sw_utilities.sw_utility_dfconcat_control import display_dfconcat_utility\ndisplay_dfconcat_utility(0)"
-SW_UTILS_DC_HELP_CELL       =   "<div align='left' id='DCDFConcatUtilityHelp'/><p class='DCTitleChapterstext'></p></div>"
-SW_UTILS_DC_TITLE_CELL      =   "<div align='left' id='DCDFConcatUtility'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/dataframeConcat.png' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h3>&nbsp;&nbsp;&nbsp;Dataframe Concatenation</h3></div></div>"
+SW_UTILS_DC_CODE_CELL       =   'from dfcleanser.sw_utilities.sw_utility_dfconcat_control import display_dfconcat_utility\\ndisplay_dfconcat_utility(0)'
+SW_UTILS_DC_HELP_CELL       =   '<div align="left" id="DCDFConcatUtilityHelp"/><p class="DCTitleChapterstext"></p></div>'
+SW_UTILS_DC_TITLE_CELL      =   '<div align="left" id="DCDFConcatUtility"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/dataframeConcat.png" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h3>&nbsp;&nbsp;&nbsp;Dataframe Concatenation</h3></div></div>'
 
-SW_UTILS_DB_CODE_CELL       =   "from dfcleanser.sw_utilities.sw_utility_dfsubset_control import display_dfsubset_utility\ndisplay_dfsubset_utility(0)"
-SW_UTILS_DB_HELP_CELL       =   "<div align='left' id='DCDFSubsetUtilityHelp'/><p class='DCTitleChapterstext'></p></div>"
-SW_UTILS_DB_TITLE_CELL      =   "<div align='left' id='DCDFSubsetUtility'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/dataframeSubset.png' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h3>&nbsp;&nbsp;&nbsp;Dataframe Subset</h3></div></div>"
+SW_UTILS_DB_CODE_CELL       =   'from dfcleanser.sw_utilities.sw_utility_dfsubset_control import display_dfsubset_utility\\ndisplay_dfsubset_utility(0)'
+SW_UTILS_DB_HELP_CELL       =   '<div align="left" id="DCDFSubsetUtilityHelp"/><p class="DCTitleChapterstext"></p></div>'
+SW_UTILS_DB_TITLE_CELL      =   '<div align="left" id="DCDFSubsetUtility"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/dataframeSubset.png" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h3>&nbsp;&nbsp;&nbsp;Dataframe Subset</h3></div></div>'
 
-SW_UTILS_GF_CODE_CELL       =   "from dfcleanser.sw_utilities.sw_utility_genfunc_control import display_gen_function\ndisplay_gen_function(0)"
-SW_UTILS_GF_HELP_CELL       =   "<div align='left' id='DCGenFunctionUtilityHelp'><p class='DCTitleChapterstext'></p></div>"
-SW_UTILS_GF_TITLE_CELL      =   "<div align='left' id='DCGenFunctionUtility'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/GenFunction.png' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h3>&nbsp;&nbsp;&nbsp;Generic Functions</h3></div></div>"
+SW_UTILS_GF_CODE_CELL       =   'from dfcleanser.sw_utilities.sw_utility_genfunc_control import display_gen_function\\ndisplay_gen_function(0)'
+SW_UTILS_GF_HELP_CELL       =   '<div align="left" id="DCGenFunctionUtilityHelp"><p class="DCTitleChapterstext"></p></div>'
+SW_UTILS_GF_TITLE_CELL      =   '<div align="left" id="DCGenFunctionUtility"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/GenFunction.png" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h3>&nbsp;&nbsp;&nbsp;Generic Functions</h3></div></div>'
 
-SW_UTILS_GC_CODE_CELL       =   "from dfcleanser.sw_utilities.sw_utility_geocode_control import display_geocode_utility\ndisplay_geocode_utility(0)"
-SW_UTILS_GC_HELP_CELL       =   "<div align='left' id='DCGeocodeUtilityHelp'/><p class='DCTitleChapterstext'></p></div>"
-SW_UTILS_GC_TITLE_CELL      =   "<div align='left' id='DCGeocodeUtility'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/GetLongLat.png' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h3>&nbsp;&nbsp;&nbsp;Geocoding</h3></div></div>"
+SW_UTILS_GC_CODE_CELL       =   'from dfcleanser.sw_utilities.sw_utility_geocode_control import display_geocode_utility\\ndisplay_geocode_utility(0)'
+SW_UTILS_GC_HELP_CELL       =   '<div align="left" id="DCGeocodeUtilityHelp"/><p class="DCTitleChapterstext"></p></div>'
+SW_UTILS_GC_TITLE_CELL      =   '<div align="left" id="DCGeocodeUtility"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/GetLongLat.png" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h3>&nbsp;&nbsp;&nbsp;Geocoding</h3></div></div>'
 
-SYSTEM_CODE_CELL            =   "from dfcleanser.system.system_control import display_system_environment\ndisplay_system_environment(0)"
-SYSTEM__ABBR_CODE_CELL      =   "from dfcleanser.system.system_control import display_system_environment\ndisplay_system_environment(16)"
-SYSTEM_HELP_CELL            =   "<div align='left' id='SystemHelp'/><p class='DCTitleChapterstext'></p></div>"
-SYSTEM_TITLE_CELL           =   "<div align='left' id='DCSystem'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/systemEnvironment.png' width='80' align='left'/></div><div><image width='10'></div><div><h2>&nbsp;&nbsp;&nbsp;System Environment : </h2><a name='SystemEnvironment'></a></div></div>"
+SYSTEM_CODE_CELL            =   "from dfcleanser.system.system_control import display_system_environment\\ndisplay_system_environment(0)"
+SYSTEM__ABBR_CODE_CELL      =   'from dfcleanser.system.system_control import display_system_environment\ndisplay_system_environment(16)'
+SYSTEM_HELP_CELL            =   '<div align="left" id="SystemHelp"/><p class="DCTitleChapterstext"></p></div>'
+SYSTEM_TITLE_CELL           =   '<div align="left" id="DCSystem"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/systemEnvironment.png" width="80" align="left"/></div><div><image width="10"></div><div><h2>&nbsp;&nbsp;&nbsp;System Environment : </h2><a name="SystemEnvironment"></a></div></div>'
 
-TRANSFORM_CODE_CELL         =   "from dfcleanser.data_transform.data_transform_control import display_data_transform\ndisplay_data_transform(0)"
-TRANSFORM_HELP_CELL         =   "<div align='left' id='TransformHelp'/><p class='DCTitleChapterstext'></p></div>"
-TRANSFORM_TITLE_CELL        =   "<div align='left' id='DCDataTransform'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/dataTransform.jpg' width='80' align='left' style='border:0px;margin-right:6px;margin-left:0px'/></div><div><image width='10'></div><div><image width='10'><h2>&nbsp;&nbsp;&nbsp;Data Transform</h2><a name='DataTransform'></a></div></div>"
+TRANSFORM_CODE_CELL         =   'from dfcleanser.data_transform.data_transform_control import display_data_transform\\ndisplay_data_transform(0)'
+TRANSFORM_HELP_CELL         =   '<div align="left" id="TransformHelp"/><p class="DCTitleChapterstext"></p></div>'
+TRANSFORM_TITLE_CELL        =   '<div align="left" id="DCDataTransform"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/dataTransform.jpg" width="80" align="left" style="border:0px;margin-right:6px;margin-left:0px"/></div><div><image width="10"></div><div><image width="10"><h2>&nbsp;&nbsp;&nbsp;Data Transform</h2><a name="DataTransform"></a></div></div>'
 
-WORKING_CODE_CELL           =   "# working cell- please do not remove"
-WORKING_TITLE_CELL          =   "<div align='left' id='Restricted'/><div><img src='https://rickkrasinski.github.io/dfcleanser/graphics/Restricted.jpg' width='80' align='left'/></div><div><image width='10'></div><div><image width='10'><h2>&nbsp;&nbsp;&nbsp;Restricted</h2></div></div>"
+WORKING_CODE_CELL           =   '# working cell- please do not remove'
+WORKING_TITLE_CELL          =   '<div align="left" id="Restricted"/><div><img src="https://rickkrasinski.github.io/dfcleanser/graphics/Restricted.jpg" width="80" align="left"/></div><div><image width="10"></div><div><image width="10"><h2>&nbsp;&nbsp;&nbsp;Restricted</h2></div></div>'
 
 
 """
