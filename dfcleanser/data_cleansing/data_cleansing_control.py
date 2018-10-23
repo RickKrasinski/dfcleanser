c6dd5c2d0e705cecc6b6c451d16986249c65d942
RickKrasinski <rickkrasinski@stny.rr.com>
2018-10-22 23:33:18 -0400

    Add files via upload

--- dfcleanser/data_cleansing/data_cleansing_control.py
+++ dfcleanser/data_cleansing/data_cleansing_control.py
@@ -44,6 +44,7 @@ def display_data_cleansing(option,parms=None) :
     
     if(not cfg.check_if_dc_init()) :
         dcw.display_no_data_heading()
+        dfchelp.clear_help_text(dfchelp.CLEANSE_HELP_BASE)
         
         return
     
@@ -58,6 +59,7 @@ def display_data_cleansing(option,parms=None) :
             from dfcleanser.data_inspection.data_inspection_widgets import display_inspection_data
             display_inspection_data()
             clear_data_cleansing_data()
+            dfchelp.clear_help_text(dfchelp.CLEANSE_HELP_BASE)
             
         elif(option == dcm.CHANGE_COLUMN_OPTION) :
 
@@ -351,6 +353,7 @@ def display_data_cleansing(option,parms=None) :
         
         from dfcleanser.data_inspection.data_inspection_widgets import display_inspection_data
         display_inspection_data()
+        dfchelp.clear_help_text(dfchelp.CLEANSE_HELP_BASE)
 
 """
 #--------------------------------------------------------------------------
